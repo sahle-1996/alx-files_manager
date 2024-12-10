e4ad1157d8fa43d513e14e9941e81268963e811a
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-12-10 13:46:34 +0000

    update

--- utils/redis.js
+++ utils/redis.js
@@ -1,55 +1,63 @@
-import redis from 'redis';
+import { promisify } from 'util';
+import { createClient } from 'redis';
 
-class CacheService {
+/**
+ * Represents a Redis client.
+ */
+class RedisClient {
+  /**
+   * Creates a new RedisClient instance.
+   */
   constructor() {
-    this.cache = redis.createClient();
-
-    this.cache.on('error', (err) => {
-      console.error(`Cache service error: ${err}`);
+    this.client = createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
     });
   }
 
-  isConnected() {
-    return this.cache.connected;
+  /**
+   * Checks if this client's connection to the Redis server is active.
+   * @returns {boolean}
+   */
+  isAlive() {
+    return this.isClientConnected;
   }
 
-  async fetch(key) {
-    return new Promise((resolve, reject) => {
-      this.cache.get(key, (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      });
-    });
+  /**
+   * Retrieves the value of a given key.
+   * @param {String} key The key of the item to retrieve.
+   * @returns {String | Object}
+   */
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
   }
 
-  async store(key, value, ttlInSeconds) {
-    return new Promise((resolve, reject) => {
-      this.cache.setex(key, ttlInSeconds, value, (err, result) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
-    });
+  /**
+   * Stores a key and its value along with an expiration time.
+   * @param {String} key The key of the item to store.
+   * @param {String | Number | Boolean} value The item to store.
+   * @param {Number} duration The expiration time of the item in seconds.
+   * @returns {Promise<void>}
+   */
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX)
+      .bind(this.client)(key, duration, value);
   }
 
-  async remove(key) {
-    return new Promise((resolve, reject) => {
-      this.cache.del(key, (err, result) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
-    });
+  /**
+   * Removes the value of a given key.
+   * @param {String} key The key of the item to remove.
+   * @returns {Promise<void>}
+   */
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
   }
 }
 
-const cacheService = new CacheService();
-
-export default cacheService;
+export const redisClient = new RedisClient();
+export default redisClient;
