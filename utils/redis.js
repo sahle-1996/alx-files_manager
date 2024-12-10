c40699e71e48faada3f9c5025ab9173004b7194c
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-12-10 13:33:27 +0000

    update

--- utils/redis.js
+++ utils/redis.js
@@ -1,59 +1,32 @@
-import { createClient } from 'redis';
-import { promisify } from 'util';
+const redis = require('redis');
+const { promisify } = require('util');
 
-/**
- * RedisHandler class for interacting with Redis.
- */
-class RedisHandler {
+class RedisService {
   constructor() {
-    this.redisClient = createClient();
-    this.asyncGet = promisify(this.redisClient.get).bind(this.redisClient);
-
-    this.redisClient.on('error', (err) => {
-      console.error(`Failed to connect to Redis: ${err.message}`);
-    });
-
-    this.redisClient.on('ready', () => {
-      // console.info('Redis connection established successfully');
+    this.connection = redis.createClient();
+    this.fetchAsync = promisify(this.connection.get).bind(this.connection);
+    this.connection.on('error', (err) => {
+      console.log(`Error connecting to Redis server: ${err.message}`);
     });
   }
 
-  /**
-   * Verifies if Redis connection is active.
-   * @returns {boolean} Connection status.
-   */
-  isAlive() {
-    return this.redisClient.connected;
+  isActive() {
+    return this.connection.connected;
   }
 
-  /**
-   * Fetches a value associated with a key.
-   * @param {string} key - The key to retrieve.
-   * @returns {Promise<string>} The corresponding value.
-   */
-  async getValue(key) {
-    return this.asyncGet(key);
+  async retrieve(key) {
+    return this.fetchAsync(key);
   }
 
-  /**
-   * Stores a key-value pair with a time-to-live (TTL).
-   * @param {string} key - Key to store.
-   * @param {string} value - Value to associate with the key.
-   * @param {number} ttl - Time-to-live in seconds.
-   */
-  async setValue(key, value, ttl) {
-    this.redisClient.setex(key, ttl, value);
+  async store(key, value, ttl) {
+    this.connection.setex(key, ttl, value);
   }
 
-  /**
-   * Removes a key from Redis storage.
-   * @param {string} key - The key to remove.
-   */
-  async removeKey(key) {
-    this.redisClient.del(key);
+  async remove(key) {
+    this.connection.del(key);
   }
 }
 
-const redisHandler = new RedisHandler();
+const redisService = new RedisService();
 
-export default redisHandler;
+module.exports = redisService;
